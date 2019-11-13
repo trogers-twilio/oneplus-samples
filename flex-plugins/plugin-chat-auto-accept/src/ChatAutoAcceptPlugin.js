44a437540f28c09e422aa7c0bd18e09b21db848a
Terence Rogers <trogers@twilio.com>
2019-11-12 17:15:47 -0800

    Sample for auto accepting chat tasks

---
+++ flex-plugins/plugin-chat-auto-accept/src/ChatAutoAcceptPlugin.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Actions, TaskHelper, VERSION } from '@twilio/flex-ui';
+import { FlexPlugin } from 'flex-plugin';
+
+const PLUGIN_NAME = 'ChatAutoAcceptPlugin';
+
+export default class ChatAutoAcceptPlugin extends FlexPlugin {
+  constructor() {
+    super(PLUGIN_NAME);
+  }
+
+  /**
+   * This code is run when your plugin is being started
+   * Use this to modify any UI components or attach to the actions framework
+   *
+   * @param flex { typeof import('@twilio/flex-ui') }
+   * @param manager { import('@twilio/flex-ui').Manager }
+   */
+  init(flex, manager) {
+    console.debug('Flex UI version', VERSION);
+
+    manager.workerClient.on('reservationCreated', (reservation) => {
+      const { sid, task } = reservation;
+      // This conditional check is optional. If you want all types of
+      // tasks to be auto accepted, you can remove the 'if' statement
+      if (TaskHelper.isChatBasedTask(task)) {
+        Actions.invokeAction('AcceptTask', { sid });
+        Actions.invokeAction('SelectTask', { sid });
+      }
+    });
+  }
+}
